bab2a349efabda01514b32ca50468599639301d8
YunJehyeok <dbswpgur2@naver.com>
2021-04-04 20:33:16 +0900

    4/4

--- client/src/routes/normalRoute/Search.js
+++ client/src/routes/normalRoute/Search.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { SEARCH_REQUEST } from "../../redux/types";
-import { Row } from "reactstrap";
+import { Row, Button } from "reactstrap";
 import PostCardOne from "../../components/post/PostCardOne";
 
 function Search({ theme }) {
@@ -21,12 +21,42 @@ function Search({ theme }) {
 
   return (
     <div className="mt-5 mb-5">
-      <h1 className="mb-5">검색결과: "{searchTerm}"</h1>
+      <Row
+        className="d-flex justify-content-center mt-5 py-2 mb-5 sticky-top rounded"
+        style={
+          theme === "dark" ? style.darkCategoryBox : style.lightCategoryBox
+        }
+      >
+        <div className="mr-3">
+          <span style={{ fontSize: "1.5rem" }}>
+            Search Result for <b>{searchTerm}</b>
+          </span>
+        </div>
+      </Row>
       <Row>
         <PostCardOne posts={searchResult} theme={theme} />
       </Row>
     </div>
   );
 }
+
+const style = {
+  darkCategoryBox: {
+    width: "94%",
+    marginLeft: "3%",
+    backgroundColor: "white",
+    color: "#212529",
+    borderLeft: "4px solid gray",
+    borderRight: "4px solid gray",
+  },
+  lightCategoryBox: {
+    width: "94%",
+    marginLeft: "3%",
+    backgroundColor: "#212529",
+    color: "white",
+    borderLeft: "4px solid gray",
+    borderRight: "4px solid gray",
+  },
+};
 
 export default Search;
